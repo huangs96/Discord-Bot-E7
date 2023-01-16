4feccde84c43eb258f005dedc568fbf449d5fc8e
Stephen <stephen_1996@live.ca>
2023-01-16 13:06:43 -0800

    created googlesheets class with methods for updating google sheets

---
+++ googleAuth/googleSheet.js
@@ -0,0 +1,53 @@
+const googleAuth = require('./GoogleAuthSingleton.js');
+const ClassWithImmutablePublicAttr = require('/home/runner/Discord-Bot-E7/BaseClasses/ClassWithImmutablePublicAttr.js');
+const ProtectedScope = require('/home/runner/Discord-Bot-E7/ClassExtensions/ProtectedScope.js');
+
+const {auth, sheet} = googleAuth;
+
+const GoogleSheet = (() => {
+  const sharedProtected = ProtectedScope();
+
+  return class GoogleSheet extends ClassWithImmutablePublicAttr {
+    constructor(spreadsheetId) {
+      super();
+
+      const set = sharedProtected(this).DefineImmutablePublic;
+
+      set('spreadsheetId', spreadsheetId);
+      set('Update', this.#Update);
+      set('GetMetaData', this.#GetMetaData);
+      set('ReadData', this.#ReadData);
+    }
+
+    #Update(range, values) {
+      return sheet.spreadsheets.values.update({
+        auth,
+        spreadsheetId: this.spreadsheetId,
+        range: range,
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+          values: values
+        }
+      });
+    }
+  
+    async #GetMetaData() {
+      return await sheet.spreadsheets.get({
+        auth,
+        spreadsheetId: this.spreadsheetId,
+      })
+    };
+  
+    async #ReadData(range) {
+      const result = await sheet.spreadsheets.values.get({
+        auth,
+        spreadsheetId: this.spreadsheetId,
+        range: range
+      });
+  
+      return result.data.values;
+    }
+  }
+})();
+
+module.exports = GoogleSheet;
