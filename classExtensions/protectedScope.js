8fa545f0ef7bd7e14bd0e32495d6ec13111c9b69
Stephen <stephen_1996@live.ca>
2023-01-16 12:57:50 -0800

    added hashmap functionality to get or set container

---
+++ classExtensions/protectedScope.js
@@ -0,0 +1,21 @@
+const sharedScope = new WeakMap();
+
+//Extends hashmap functionality so user doesn't need to do container.get or container.set
+//Example: container(this) can either get or set the value for this within the hashmap
+const sharedScopeAccess = (container) => {
+    
+    return function(context) {
+        if (!container.has(context)) {
+            container.set(context, {});
+        }
+        return container.get(context);
+    };
+    
+};
+
+const createSharedScope = () => {
+    return sharedScopeAccess(sharedScope);
+};
+
+
+module.exports = createSharedScope;
